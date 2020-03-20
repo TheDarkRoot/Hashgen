a73dd151a5272eef34b55f72c230e61c1dec05be
TheDarkRoot <TheDarkRoot@users.noreply.github.com>
2020-03-20 18:10:21 +0300

    Hashgen
    Generate more 39 type hash.

--- Hashgen.py
+++ Hashgen.py
@@ -41,7 +41,6 @@ elif sys.platform == "win32":
 try:
 	import passlib , progressbar
 except ImportError:
-        banner()
 	time.sleep(0.5)
         print (BB+"["+WW+"="+BB+"] "+GG+"installing module "+RR+"progressbar, passlib.\n"+W)
 
